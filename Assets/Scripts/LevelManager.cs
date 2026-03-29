d47a3f8bc70764662e27c8047d5dda55a945b565
noahwc <noahwcheng@gmail.com>
2026-03-28 21:37:48 -0400

    LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private int drivesToCollect = 3;
+
+    [SerializeField]
+    private GameObject laserParticles;
+
+    [SerializeField]
+    private TextMeshPro computerText;
+
+    private int drivesCollected = 0;
+
+    private void EndGame()
+    {
+        if (drivesCollected >= drivesToCollect)
+        {
+            Victory();
+        } else
+        {
+            Defeat();
+        }
+    }
+
+    private void Victory()
+    {
+        laserParticles.SetActive(true);
+        computerText.text = "Congratulations.";
+    }
+
+    private void Defeat()
+    {
+        computerText.text = "I'm sorry...";
+    }
+}
